151301ac938e73f05ae16d87c7fa22be5886f253
George-Bogdan Ivanov <ivanov.george.bogdan@gmail.com>
2017-04-14 13:57:14 +0100

    Adding a testing file

---
+++ main.py
@@ -0,0 +1,8 @@
+from newspaper import Article
+
+
+article = Article('http://fox13now.com/2013/12/30/new-year-new-laws-obamacare-pot-guns-and-drones/')
+article.download()
+article.parse()
+
+print(article.text)
